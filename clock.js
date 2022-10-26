2ba7baa9ecfb0e097431accc598274005ebdb644
Chucks Nwosu <chucksn611@gmail.com>
2022-10-26 16:06:29 -0500

    clock v1.0.1(adjust button added)

--- clock.js
+++ clock.js
@@ -1,14 +1,9 @@
 const secondsStick = document.querySelector(".seconds-stick");
 const minuteStick = document.querySelector(".minute-stick");
 const hourStick = document.querySelector(".hour-stick");
-const todaysDate = new Date();
-let h = todaysDate.getHours();
-let m = todaysDate.getMinutes();
-let s = todaysDate.getSeconds();
 let secStickMove = 0;
 let minStickMove = 0;
 let hrStickMove = 0;
-let clicked = false;
 
 setInterval(() => {
   // seconds stick movement
@@ -38,42 +33,52 @@ setInterval(() => {
   }
 }, 1000);
 
+//event on clock adjust and setting buttons
 let settings = document.querySelector(".clock-setting-container");
 
-settings.addEventListener("mousedown", (e) => {
-  let mouseDownEvent = setInterval(() => {
-    if (e.target.classList.contains("adjust-clockwise")) {
-      if (minStickMove < 354) {
-        minStickMove += 6;
-      } else {
-        minStickMove = 0;
-      }
-      minuteStick.style.transform = `rotate(${minStickMove}deg)`;
-      if (hrStickMove < 354) {
-        hrStickMove += 0.5;
-      } else {
-        hrStickMove = 0;
-      }
-      hourStick.style.transform = `rotate(${hrStickMove}deg)`;
+let settingEventFunc = (e) => {
+  if (e.target.classList.contains("adjust-clockwise")) {
+    if (minStickMove < 354) {
+      minStickMove += 6;
+    } else {
+      minStickMove = 0;
     }
-
-    if (e.target.classList.contains("adjust-anticlockwise")) {
-      if (minStickMove < 354) {
-        minStickMove -= 6;
-      } else {
-        minStickMove = 0;
-      }
-      minuteStick.style.transform = `rotate(${minStickMove}deg)`;
-      if (hrStickMove < 354) {
-        hrStickMove -= 0.5;
-      } else {
-        hrStickMove = 0;
-      }
-      hourStick.style.transform = `rotate(${hrStickMove}deg)`;
+    minuteStick.style.transform = `rotate(${minStickMove}deg)`;
+    if (hrStickMove < 354) {
+      hrStickMove += 0.5;
+    } else {
+      hrStickMove = 0;
     }
+    hourStick.style.transform = `rotate(${hrStickMove}deg)`;
+  }
 
-    settings.addEventListener("mouseup", () => {
-      clearInterval(mouseDownEvent);
-    });
-  }, 400);
+  if (e.target.classList.contains("adjust-anticlockwise")) {
+    if (minStickMove < 354) {
+      minStickMove -= 6;
+    } else {
+      minStickMove = 0;
+    }
+    minuteStick.style.transform = `rotate(${minStickMove}deg)`;
+    if (hrStickMove < 354) {
+      hrStickMove -= 0.5;
+    } else {
+      hrStickMove = 0;
+    }
+    hourStick.style.transform = `rotate(${hrStickMove}deg)`;
+  }
+
+  if (e.target.classList.contains("adjust-done")) {
+    settings.style.display = "none";
+    adjust.style.display = "flex";
+  }
+};
+
+settings.addEventListener("click", settingEventFunc);
+
+let adjust = document.querySelector(".clock-adjust-container");
+adjust.addEventListener("click", (e) => {
+  if (e.target.classList.contains("adjust-clock")) {
+    adjust.style.display = "none";
+    settings.style.display = "flex";
+  }
 });
